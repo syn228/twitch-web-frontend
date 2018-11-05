c59a925aa123d7c9ac88c7f6ebdd09065f8e40b8
Aaron Na <syn228@nyu.edu>
2018-11-04 20:43:46 -0800

    signup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,7 @@ class Navbar extends Component {
         <NavLink style={{marginRight: "10px", color: "white", fontWeight: "bold"}} exact to="/following" >Following</NavLink>
         <NavLink style={{marginRight: "10px", color: "white", fontWeight: "bold"}} exact to="/browse">Browse</NavLink>
         <Search/>
+        <NavLink style={{position: "absolute", color: "white", fontWeight: "bold", marginLeft: "75%"}} exact to="/login">Sign Up</NavLink>
         <NavLink style={{position: "absolute", color: "white", fontWeight: "bold", marginLeft: "80%"}} exact to="/login">Log-In</NavLink>
       </div>
     )
